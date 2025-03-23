bcdbc23c784ae3c9bb13d047dba9d3efd0ff0b56
Alexandr Syrykh <alexandr.syryh@gmail.com>
2025-03-23 19:10:39 +0300

    Added music wall and floors on the level

--- XYZRoguelike/DeveloperLevel.h
+++ XYZRoguelike/DeveloperLevel.h
@@ -4,6 +4,9 @@
 #include <array>
 #include "Scene.h"
 #include "Player.h"
+#include "Music.h"
+#include "Floor.h"
+#include "Wall.h"
 
 using namespace XYZEngine;
 
@@ -17,5 +20,9 @@ namespace XYZRoguelike
 		void Stop() override;
 	private:
 		std::shared_ptr<Player> player;
+		std::unique_ptr<Music> music;
+
+		std::vector<std::unique_ptr<Wall>> walls;
+		std::vector<std::unique_ptr<Floor>> floors;
 	};
 }
