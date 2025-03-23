5806509bb0c296bfce15dd8ac3843ebc717f6704
Alexandr Syrykh <alexandr.syryh@gmail.com>
2025-03-23 19:07:14 +0300

    Added Logger

--- XYZEngine/Engine.cpp
+++ XYZEngine/Engine.cpp
@@ -16,12 +16,16 @@ namespace XYZEngine
 	{
 		unsigned int seed = (unsigned int)time(nullptr);
 		srand(seed);
+
+		setupLogger();
 	}
 
 	void Engine::Run()
 	{
 		sf::Clock gameClock;
 		sf::Event event;
+
+		LOG_INFO("Program was started!");
 		
 		while (RenderSystem::Instance()->GetMainWindow().isOpen())
 		{
@@ -50,5 +54,15 @@ namespace XYZEngine
 
 			RenderSystem::Instance()->GetMainWindow().display();
 		}
+	}
+
+	void Engine::setupLogger()
+	{
+		auto logger = std::make_shared<Logger>();
+		logger->addSink(std::make_shared<ConsoleSink>());
+		logger->addSink(std::make_shared<FileSink>("log.txt"));
+
+		LoggerRegistry::getInstance().registerLogger("global", logger);
+		LoggerRegistry::getInstance().setDefaultLogger(logger);
 	}
 }
